d4252adc4765b446f38f8bed9245be2c1dd88363
CatLearned <CatLearned>
2015-12-07 21:32:31 +0300

    Added Ways, Subways. Some bugfixes. New look interface

--- constantstypes.pas
+++ constantstypes.pas
@@ -2,17 +2,23 @@ unit ConstantsTypes;
 {$mode objfpc}{$H+}
 interface
 uses
-  Classes, SysUtils, Station;
+  Classes, SysUtils, Station, way;
 const r=5; // radius of stations
       mincr=1;
       maxcr=5;
       minway=1;
       maxway=30;
 type
-  PItem = ^Item;
-  Item = record
+  PItem = ^SItem;
+  SItem = record
     Value:Tstation;
     next:PItem;
+  end;
+type
+  WPitem = ^WayItem;
+  WayItem = record
+    Value:TWay;
+    next:WPitem;
   end;
 
 implementation
--- unit1.pas
+++ unit1.pas
@@ -18,6 +18,7 @@ type
     AddStationItem: TMenuItem;
     AddWayItem: TMenuItem;
     AddCrossingItem: TMenuItem;
+    Image1: TImage;
     UnderGroundMap: TImage;
     MinWayLabel: TLabel;
     AddCrossingPanel: TPanel;
@@ -45,23 +46,33 @@ type
     OpenFileItem: TMenuItem;
     AddStationButton: TToggleBox;
     TimeCrossingLabel: TLabel;
+    procedure AddCrossingButtonClick(Sender: TObject);
     procedure AddCrossingItemClick(Sender: TObject);
-    procedure AddStationButtonChange(Sender: TObject);
     procedure AddStationItemClick(Sender: TObject);
     procedure AddWayItemClick(Sender: TObject);
     procedure Edit1Click(Sender: TObject);
     procedure FormCreate(Sender: TObject);
+    {Station}
+    procedure AddStationButtonChange(Sender: TObject);
     procedure UndergroundMapClick(Sender: TObject);
     Procedure AddStationList(v:TStation);
     Function CheckStationName(s:string):boolean;
     Function CheckCoords(x,y,r:integer):boolean;
-    function GetStation(s:string):TStation;
+    function GetStation(s:string):PItem;
+    {Way}
+    procedure AddWayButtonClick(Sender: TObject);
+    Procedure AddWayList(v:TWay);
   private
-     Redactor:boolean;
-     Stationlist:Item;
-     Head:PItem;
-     _station:TStation;
-     P:PItem;
+    {Переменные для Station}
+    _station:TStation;
+    Stationlist:SItem;
+    Head:PItem;
+    P:PItem;
+    {Переменные для Way}
+    _way:TWay;
+    WayList:WayItem;
+    WayHead:WPItem;
+    WP:WPItem;
     { private declarations }
   public
     { public declarations }
@@ -82,9 +93,11 @@ begin
 //  Stationlist.Create;
   _station.Create;
   Head:=nil;
+  _way.create;
+  WayHead:=nil;
 end;
 
-procedure TForm1.UndergroundMapClick(Sender: TObject); // Рисование на Canvas и создание объекта "станция".
+procedure TForm1.UndergroundMapClick(Sender: TObject); // Рисование на Canvas и создание объекта "станция". Добавление Объекта станция в список с использованием процедуры.
 var
   coord: TPoint;
 begin
@@ -114,6 +127,49 @@ begin
   AddStationPanel.Visible:=true;
   AddWayPanel.Visible:=False;
   AddCrossingPanel.Visible:=False;
+  AddStationButton.Checked:=False;
+end;
+
+procedure TForm1.AddWayButtonClick(Sender: TObject);
+begin
+  if not (NameOfStationFirstField.Text='') and not (NameOfStationSecondField.Text='') then
+  Begin
+    if not (NameOfStationFirstField.Text=NameOfStationSecondField.Text) then
+    begin
+      if not CheckStationName(NameOfStationFirstField.Text) and not CheckStationName(NameOfStationSecondField.Text) then
+      begin
+        if GetStation(NameOfStationFirstField.Text)^.Value.CheckSWays
+           and
+           GetStation(NameOfStationSecondField.Text)^.Value.CheckSWays then
+        begin
+          _way.SetFStation(GetStation(NameOfStationFirstField.Text)^.Value);
+          GetStation(NameOfStationFirstField.Text)^.Value.AddSWay;
+          _way.SetSStation(GetStation(NameOfStationSecondField.Text)^.Value);
+          GetStation(NameOfStationSecondField.Text)^.Value.AddSWay;
+          _way.SetTime(TimeWayEdit.Value);
+          _way.SetTransition(True);
+          _way.SetVisited(False);
+          AddWayList(_way);
+          Form1.UndergroundMap.Canvas.MoveTo(_way.GetFStation.GetXCoord,_way.GetFStation.GetYCoord);
+          Form1.UndergroundMap.Canvas.LineTo(_way.GetSStation.GetXCoord,_way.GetSStation.GetYCoord);
+          Form1.UndergroundMap.Canvas.TextOut((_way.GetFStation.GetXCoord+_way.GetSStation.GetXCoord) div 2,
+                                              (_way.GetFStation.GetYCoord+_way.GetSStation.GetYCoord) div 2,
+                                              inttostr(_way.GetTime));
+          ShowMessage(_way.GetFStation.GetName
+                    + _way.GetSStation.GetName
+                    + inttostr(_way.GetTime)
+                    + booltostr(GetStation(NameOfStationFirstField.Text)^.Value.CheckSWays,'True','False')
+                    + inttostr(GetStation(NameOfStationFirstField.Text)^.Value.NumSWays)
+                    + BoolToStr(GetStation(NameOfStationSecondField.Text)^.Value.CheckSWays, 'True', 'False')
+                    + inttostr(GetStation(NameOfStationSecondField.Text)^.Value.NumSWays));
+        end
+        else ShowMessage('У одной из станций исчерпан лимит путей. Попробуйте создать переход.');
+      end
+      else ShowMessage('Станции пути должны существовать');
+    end
+    else ShowMessage('Названия станций пути не могут совпадать');
+  end
+  else ShowMessage('Названия станций пути не могут быть пустыми');
 end;
 
 procedure TForm1.AddWayItemClick(Sender: TObject);
@@ -121,6 +177,7 @@ begin
   AddStationPanel.Visible:=False;
   AddWayPanel.Visible:=True;
   AddCrossingPanel.Visible:=False;
+  AddStationButton.Checked:=False;
 end;
 
 
@@ -133,6 +190,36 @@ begin
   AddStationPanel.Visible:=False;
   AddWayPanel.Visible:=False;
   AddCrossingPanel.Visible:=True;
+  AddStationButton.Checked:=False;
+end;
+
+procedure TForm1.AddCrossingButtonClick(Sender: TObject);
+begin
+  if not (NameOfStationFirstCrossingField.Text='') and not (NameOfStationSecondCrossingField.Text='') then
+  Begin
+    if not (NameOfStationFirstCrossingField.Text=NameOfStationSecondCrossingField.Text) then
+    begin
+      if not CheckStationName(NameOfStationFirstCrossingField.Text) or not CheckStationName(NameOfStationSecondCrossingField.Text) then
+      begin
+        _way.SetFStation(GetStation(NameOfStationFirstCrossingField.Text)^.Value);
+        _way.SetSStation(GetStation(NameOfStationSecondCrossingField.Text)^.Value);
+        _way.SetTime(TimeCrossingEdit.Value);
+        _way.SetTransition(False);
+        _way.SetVisited(False);
+        AddWayList(_way);
+        //RISOVANIE
+        Form1.UndergroundMap.Canvas.Pen.Style:= psDot;
+        Form1.UndergroundMap.Canvas.MoveTo(_way.GetFStation.GetXCoord,_way.GetFStation.GetYCoord);
+        Form1.UndergroundMap.Canvas.LineTo(_way.GetSStation.GetXCoord,_way.GetSStation.GetYCoord);
+        Form1.UndergroundMap.Canvas.TextOut((_way.GetFStation.GetXCoord+_way.GetSStation.GetXCoord) div 2,(_way.GetFStation.GetYCoord+_way.GetSStation.GetYCoord) div 2,inttostr(_way.GetTime));
+        Form1.UndergroundMap.Canvas.Pen.Style:= psSolid;
+        //DOBAVIT'
+      end
+      else ShowMessage('Станции перехода должны существовать');
+    end
+    else ShowMessage('Названия станций перехода не могут совпадать');
+  end
+  else ShowMessage('Названия станций перехода не могут быть пустыми');
 end;
 
 procedure TForm1.AddStationButtonChange(Sender: TObject);
@@ -183,17 +270,38 @@ begin
   head:=standart;
 end;
 
-function TForm1.GetStation(s:string):TStation;
+function TForm1.GetStation(s:string):PItem;
   var standart:PItem;
 begin
   standart:=head;
   while Head<>nil do
   begin
     p:=Head;
-    if p^.Value.GetName=s then GetStation:=p^.Value;
+    if p^.Value.GetName=s then GetStation:=p;
     Head:=Head^.next;
   end;
   head:=standart;
+end;
+
+//Function TForm1.GetItemStation(s:string):PItem;
+//  var standart:PItem;
+//begin
+//  standart:=head;
+//  while Head<>nil do
+//  begin
+//    p:=Head;
+//    if p^.Value.GetName=s then GetItemStation:=p;
+//    Head:=Head^.next;
+//  end;
+//  head:=standart;
+//end;
+
+Procedure TForm1.AddWayList(v:TWay);
+begin
+  New(WP);
+  WP^.value:=v;
+  WP^.next:=WayHead;
+  WayHead:=WP;
 end;
 
 end.
